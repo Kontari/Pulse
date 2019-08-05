a66bb1595941ef6a3ff7c004ad76b649c186c3b1
Jason Walter <jaason@protonmail.ch>
2019-08-05 12:33:05 -0400

    Built the C monitor functions into a Python module, see the readme under monitoring for details on use

--- monitoring/monitor_module.c
+++ monitoring/monitor_module.c
@@ -1,27 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
+#define PY_SSIZE_T_CLEAN
+#include <python3.7m/Python.h>  // includes favorites such as stdio, stdlib, and string
 #include <unistd.h>
 #include <dirent.h>
-#include <string.h>
-#include <ncurses.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-
-/* Handy-dandy structure containing CPU load averages for
-1, 5, and 15 minutes */
-struct loadavgs {
-    float min1_avg;
-    float min5_avg;
-    float min15_avg;
-};
-
-/* Handy-dandy structure containing info on total, free, and
-available memory in kilobytes */
-struct meminfo {
-    unsigned int memtotal;
-    unsigned int memfree;
-    unsigned int memavailable;
-};
 
 /* Standard error function because I'm lazy */
 void frick(char *msg) {
@@ -29,20 +11,21 @@ void frick(char *msg) {
     exit(-1);
 }
 
-/*This guy returns information on the system's total, free, and available memory
-from /proc/meminfo in a handy-dandy structure */
-struct meminfo get_meminfo(void) {
-    struct meminfo meminfo;
+/*This guy returns information on the system's total, free, and available memory,
+in kilobytes, from /proc/meminfo in a Python tuple, and in that order */
+static PyObject *get_meminfo(PyObject *self) {
+    unsigned int memtotal, memfree, memavailable;
     FILE *fp = fopen("/proc/meminfo", "r");
 
-    fscanf(fp, "%*s %d %*s %*s %d %*s %*s %d", &meminfo.memtotal, &meminfo.memfree, &meminfo.memavailable);
+    fscanf(fp, "%*s %d %*s %*s %d %*s %*s %d", &memtotal, &memfree, &memavailable);
     fclose(fp);
-    return meminfo;
+    //printf("%d %d %d", meminfo.memtotal, meminfo.memfree, meminfo.memavailable);
+    return Py_BuildValue("iii", memtotal, memfree, memavailable);
 }
 
 /* This guy returns the major and minor number of the device
-on which root is mounted, in hexadecimal. The dev_t type is a 
-short that thinks it's special. */
+on which root is mounted, in hexadecimal. The dev_t type is an
+unsigned long int that thinks it's special. */
 dev_t get_file_devid(char *path) {
     struct stat filestat;
     char hex_str[8];    // For formatting
@@ -70,7 +53,7 @@ dev_t dev_t_conversion(char *dec) {
 }
 
 /* This guy reads the current disk I/O from /proc/diskstats */
-int get_disk_io(void) {
+static PyObject *get_disk_io(PyObject *self) {
     char line[128];
     int io;
     FILE *fp = fopen("/proc/diskstats", "r");
@@ -82,7 +65,7 @@ int get_disk_io(void) {
             sscanf(line, "%*d %*d %*s %*d %*d %*d %*d %*d %*d %*d %*d %d", &io);   // Gotta love format specifiers
             fclose(fp);
             fclose(null);
-            return io;
+            return Py_BuildValue("i", io);
         }
     }
 
@@ -92,7 +75,7 @@ int get_disk_io(void) {
 }
 
 /* Self explanatory function; counts processes in /proc */
-int get_process_count(void) {
+static PyObject *get_process_count(PyObject *self) {
     int process_count = 0;
     struct dirent *open;
     DIR *dirp;
@@ -103,39 +86,41 @@ int get_process_count(void) {
         strncmp(open->d_name, "2", 1) == 0 || strncmp(open->d_name, "3", 1) == 0 || strncmp(open->d_name, "4", 1) == 0 || strncmp(open->d_name, "5", 1) == 0 ||
         strncmp(open->d_name, "6", 1) == 0 || strncmp(open->d_name, "7", 1) == 0 || strncmp(open->d_name, "8", 1) == 0 || strncmp(open->d_name, "9", 1) == 0)) process_count++;
 
-    return process_count;
+    return Py_BuildValue("i", process_count);
 }
 
 /* This guy returns the first three values of /proc/loadavg
-in a handy-dandy structure. They represent the number of jobs
+in a handy-dandy Python tuple. They represent the number of jobs
 in the run queue or waiting for disk I/O averaged over 1, 5,
 and 15 minutes, respectively. */
-struct loadavgs get_loadavg(void) {
-    struct loadavgs loadavgs;
+static PyObject *get_loadavg(PyObject *self) {
+    float min1_avg, min5_avg, min15_avg;
     FILE *fp = fopen("/proc/loadavg", "r");
 
-    fscanf(fp, "%f %f %f", &loadavgs.min1_avg, &loadavgs.min5_avg, &loadavgs.min15_avg);
+    fscanf(fp, "%f %f %f", &min1_avg, &min5_avg, &min15_avg);
     fclose(fp);
-    return loadavgs;
+    return Py_BuildValue("fff", min1_avg, min5_avg, min15_avg);
 }
 
-int main(void) {
-    initscr();  // Begin curses mode
-    struct loadavgs loadavgs;
-    struct meminfo meminfo;
-    for(;;) {
-        clear();
-        loadavgs = get_loadavg();
-        meminfo = get_meminfo();
-        printw("Load averages:\t%.2f, %.2f, %.2f\n", loadavgs.min1_avg, loadavgs.min5_avg, loadavgs.min15_avg);
-        printw("Memory Info:\t%d Mb Total, %d Mb Free, %d Mb Available\n", meminfo.memtotal/1000, meminfo.memfree/1000, meminfo.memavailable/1000);
-        printw("Disk I/O:\t%d\n", get_disk_io());
-        printw("Process count:\t%d\n", get_process_count());
-        refresh();
-        sleep(3);   // Wait 3 seconds before refreshing values
-    }
+/* An array of functions in the monitor module */
+static PyMethodDef monitor_funcs[] = {
+    {"get_meminfo", (PyCFunction)get_meminfo, METH_NOARGS, "Returns free, total, and available memory"},
+    {"get_disk_io", (PyCFunction)get_disk_io, METH_NOARGS, "Returns current number of disk I/Os"},
+    {"get_process_count", (PyCFunction)get_process_count, METH_NOARGS, "Returns the current number of processes from /proc"},
+    {"get_loadavg", (PyCFunction)get_loadavg, METH_NOARGS, "Returns 1, 5, and 15 minute CPU load averages"},
+    {NULL, NULL, 0, NULL}   // Necessary to terminate the method mapping table
+};
 
-    /* Considering that these will never run, this program probably has a memory leak */
-    endwin();   // Free the memory allocated by ncurses
-    exit(0);
+/* Module definition structure */
+static struct PyModuleDef monitor_module = {
+    PyModuleDef_HEAD_INIT,
+    "monitor",
+    NULL,
+    -1,
+    monitor_funcs
+};
+
+/* Module initialization */
+PyMODINIT_FUNC PyInit_monitor(void) {
+    return PyModule_Create(&monitor_module);
 }
